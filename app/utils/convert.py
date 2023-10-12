977832b3183801cf7325e066a66a108be95d9c6c
Gino Rey <gino.rey@outlook.com>
2023-10-12 02:57:10 -0400

    lol

--- app/utils/convert.py
+++ app/utils/convert.py
@@ -17,5 +17,5 @@ def convert_mesh_to_glb(mesh: List[Image.Image], prompt: str):
     :return: A glb file
     """
     glb = export_to_ply(mesh[0], f"{prompt}.glb")
-    trimesh = trimesh.load(glb)
-    return trimesh.export(f"{prompt}.glb", file_type="glb")
+    loaded = trimesh.load(glb)
+    return loaded.export(file_obj=app/temp, file_type="glb")
